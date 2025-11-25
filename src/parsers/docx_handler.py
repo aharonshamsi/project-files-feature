8cd0195f4c133738a34eeb1cf630d0d951058911
aharonshamsi <aharonshamsi@gmail.com>
2025-11-25 02:25:31 +0200

    feat: continue processing tables

--- src/parsers/docx_handler.py
+++ src/parsers/docx_handler.py
@@ -2,9 +2,29 @@ from docx import Document
 import json
 
 
+
+def extract_table(table):
+    extracted = []
+    
+    for row in table.rows:
+        extracted_row = [cell.text.strip() for cell in row.cells]
+        extracted.append(extracted_row)
+
+    if extracted:
+        return {
+            "columns": extracted[0],
+            "rows": extracted[1:]
+        }
+    else:
+        return {"columns": [], "rows": []}
+
+    
+
+
+
 def parse_docx_file(file_path_input, file_path_output):
             
-    result = { "lessons": [] }
+    result = { "sections": [] }
 
     current_section = None 
     found_heading = False 
@@ -20,14 +40,17 @@ def parse_docx_file(file_path_input, file_path_output):
             if not text:
                 continue
 
-            if 'Heading' in style or 'title' in style:
+
+            if 'Heading' in style or 'title' in style \
+                or all(run.bold for run in para.runs if run.text.strip()): # Check title if it is bold
+
                 found_heading = True
 
                 new_section = {
                     "title": text,
                     "Paragraph": ""
                 }
-                result["lessons"].append(new_section)
+                result["sections"].append(new_section)
                 current_section = new_section
 
 
@@ -41,14 +64,26 @@ def parse_docx_file(file_path_input, file_path_output):
                 else:
                     if current_section is None:
                         current_section = {"Paragraph": ""}
-                        result["lessons"].append(current_section)
+                        result["sections"].append(current_section)
 
                     current_section["Paragraph"] += text
+        
+        
+
+        # Table handling
+        if doc.tables:
+            result["tables"] = []
+            for table in doc.tables:
+                table_data = extract_table(table)
+                result["tables"].append(table_data)
+
+
 
 
         # Create JSON file
         with open(file_path_output, 'w', encoding='utf-8') as file:
             json.dump(result, file, ensure_ascii=False, indent=4)
+
 
         
     # Error detection
@@ -63,7 +98,6 @@ def parse_docx_file(file_path_input, file_path_output):
 
     except Exception as e:
         print(f"Error: {e}")
-
 
 
 
