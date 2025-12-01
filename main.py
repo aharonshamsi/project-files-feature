23fae7671a064da634dd3e07e41c5120d242c97c
aharonshamsi <aharonshamsi@gmail.com>
2025-12-01 02:25:26 +0200

    Add: Finishing metadata, and links, and starting work on images

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from src.parsers.pdf_handler import extract_basic_text_to_json
 def main():
 
    
-    input_pdf_path = "./data/inputs/exported_document.pdf"
-    output_json_path = "data/outputs/pdf.json"
+    # input_pdf_path = "./data/inputs/hashmal.pdf"
+    # output_json_path = "data/outputs/pdf.json"
 
-    extract_basic_text_to_json(input_pdf_path, output_json_path)
+    # extract_basic_text_to_json(input_pdf_path, output_json_path)
 
 
-    #file_path_input = "data/inputs/lesson_1.docx"
-    #file_path_input = "data/input/Ex1.docx"
-    file_path_input = "data/inputs/5 Things to Do Every Day to Be Happy.docx"
+
+    #file_path_input = "data/inputs/regulations_occupational-safety-officials-construction-site.docx"
+    file_path_input = "data/inputs/hashmal-doc.docx"
     file_path_output = "data/outputs/docx.json"
 
     parsed_data = extract_docx_file_to_json(file_path_input, file_path_output)
--- src/parsers/docx_handler.py
+++ src/parsers/docx_handler.py
@@ -2,11 +2,34 @@ from docx import Document
 from docx.text.paragraph import Paragraph
 from docx.table import Table
 import json
-import os
+import re
+from docx.oxml.ns import qn
+import base64
+
+
+
 
 from src.parsers.utils import file_size_check
 
 MAX_FILE_DOCX_SIZE_BYTES = 20 * 1024 * 1024 # Size of file docx 20 MB 
+
+
+
+
+def extract_document_metadata(file_object):
+
+    properties = file_object.core_properties
+
+    metadata = {
+        "title": properties.title, 
+        "author": properties.author,
+        "subject": properties.subject,
+        "keywords": properties.keywords,
+        "creation_date": str(properties.created),
+    }
+
+    return metadata
+
 
 
 
@@ -26,6 +49,7 @@ def extract_table(table):
 
 
 
+
 def iteration_block_items(parent):
 
     for child in parent.element.body:
@@ -38,13 +62,76 @@ def iteration_block_items(parent):
 
 
 
+def paragraph_contains_image(paragraph):
+    images = []
+
+    for run in paragraph.runs:
+        drawing_elements = run._r.findall('.//w:drawing', paragraph._element.nsmap)
+        
+        if drawing_elements:
+            images.append(run)
+    
+    return images
+
+
+
+
+def extract_urls_from_text(text):
+    pattern = r'https?://[^\s)]+'
+    return re.findall(pattern, text)
+
+
+
+def handle_embedded_image(doc ,block):
+
+    for run in block.runs:
+        image = []
+        ns = run._element.nsmap
+
+        # איתור אלמנטי drawing
+        drawings = run._element.findall('.//w:drawing', namespaces=ns)
+        if not drawings:
+            continue
+
+        # טיפול בכל תמונה בתוך ה-drawing
+        for draw in drawings:
+            blip = draw.find('.//a:blip', namespaces=ns)
+            if blip is None:
+                continue
+
+            rId = blip.get(qn('r:embed'))
+            image_part = doc.part.related_parts[rId]
+            blob = image_part.blob
+
+            image = {
+                "type": "image",
+                "name": image_part.partname.split("/")[-1]
+                #"data_base64": base64.b64encode(blob).decode("utf-8")
+            }
+
+            return image
+
+
+
+
+
+
+
 def extract_docx_file_to_json(file_path_input, file_path_output):
 
-    result = {"content": []}
+    result = {"metadata": [],
+              "content": [],
+              }
 
     try:
         file_size_check(file_path_input, MAX_FILE_DOCX_SIZE_BYTES) # Import
         doc = Document(file_path_input)
+
+        metadata = extract_document_metadata(doc)
+        result["metadata"].append(metadata)
+
+        new_paragraph = False 
+
 
         for block in iteration_block_items(doc):
 
@@ -55,21 +142,52 @@ def extract_docx_file_to_json(file_path_input, file_path_output):
 
                 if not text:
                     continue
+                
+                # Url
+                urls = extract_urls_from_text(text)
+                if urls:
+                    for url in urls:
+                        result["content"].append({
+                            "type": "url",
+                            "text": url
+                        })
+                    continue
 
+
+                # Images of embedded
+                object_image = handle_embedded_image(doc, block)
+                if object_image:
+                    result["content"].append(object_image)
+
+                
                 # Heading detection
                 if 'Heading' in style or 'title' in style or \
                    all(run.bold for run in block.runs if run.text.strip()):
+                    new_paragraph = True
+                    
 
                     result["content"].append({
                         "type": "heading",
                         "text": text
                     })
 
+
                 else:
-                    result["content"].append({
-                        "type": "paragraph",
-                        "text": text
-                    })
+                    # New paragraph
+                    if new_paragraph or not result["content"] or result["content"][-1]["type"] != "paragraph":
+                        result["content"].append({
+                            "type": "paragraph",
+                            "text": text
+                        })
+
+                    else:
+                        result["content"][-1]["text"] += "\n\n" + text
+                    
+                new_paragraph = False
+
+                
+                
+
 
             # Table
             elif isinstance(block, Table):
@@ -78,6 +196,7 @@ def extract_docx_file_to_json(file_path_input, file_path_output):
                     "type": "table",
                     "data": table_data
                 })
+
 
         # Write JSON
         with open(file_path_output, "w", encoding="utf-8") as f:
@@ -95,109 +214,5 @@ def extract_docx_file_to_json(file_path_input, file_path_output):
         print(f"Error: {e}")
 
 
-
-
-
-
-# from docx import Document
-# import json
-
-
-
-# def extract_table(table):
-#     extracted = []
-    
-#     for row in table.rows:
-#         extracted_row = [cell.text.strip() for cell in row.cells]
-#         extracted.append(extracted_row)
-
-#     if extracted:
-#         return {
-#             "columns": extracted[0],
-#             "rows": extracted[1:]
-#         }
-#     else:
-#         return {"columns": [], "rows": []}
-
-    
-
-
-
-# def extract_docx_file(file_path_input, file_path_output):
-            
-#     result = { "sections": [] }
-
-#     current_section = None 
-#     found_heading = False 
-
-#     try:
-
-#         doc = Document(file_path_input)
-
-#         for para in doc.paragraphs:
-#             style = para.style.name
-#             text = para.text.strip()
-            
-#             if not text:
-#                 continue
-
-
-#             if 'Heading' in style or 'title' in style \
-#                 or all(run.bold for run in para.runs if run.text.strip()): # Check title if it is bold
-
-#                 found_heading = True
-
-#                 new_section = {
-#                     "title": text,
-#                     "Paragraph": ""
-#                 }
-#                 result["sections"].append(new_section)
-#                 current_section = new_section
-
-
-#             else:
-#                 # file with title
-#                 if found_heading:
-#                     if current_section is not None:
-#                         current_section["Paragraph"] += text
-
-#                 # file without title
-#                 else:
-#                     if current_section is None:
-#                         current_section = {"Paragraph": ""}
-#                         result["sections"].append(current_section)
-
-#                     current_section["Paragraph"] += text
-        
-        
-
-#         # Table handling
-#         if doc.tables:
-#             result["tables"] = []
-#             for table in doc.tables:
-#                 table_data = extract_table(table)
-#                 result["tables"].append(table_data)
-
-
-
-
-#         # Create JSON file
-#         with open(file_path_output, 'w', encoding='utf-8') as file:
-#             json.dump(result, file, ensure_ascii=False, indent=4)
-
-
-        
-#     # Error detection
-#     except FileNotFoundError:
-#         print("Error: File not found")
-
-#     except PermissionError:
-#         print("Error: Permission denied. Make sure the file is not open")
-
-#     except ValueError:
-#         print("Error: The file is not a valid DOCX document")
-
-#     except Exception as e:
-#         print(f"Error: {e}")
 
 
